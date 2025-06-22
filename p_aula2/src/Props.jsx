9fb1ec1bf9c7e2d461fd65315aabbfdddf4a3fff
Hitalooo <hitalo.azevedo@escolar.ifrn.edu.br>
2025-06-22 11:26:17 -0300

    .

--- p_aula2/src/Props.jsx
+++
@@ -1,9 +0,0 @@
-export default function Props(props) {
-    return(
-     <>
-        <h2>teste props</h2>
-        <p>the name {props.nome} was sent by props</p>
-        <p>{props.newname}</p>
-     </>
-    )
-}
